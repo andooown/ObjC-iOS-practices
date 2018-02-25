c4b6e2dbbcf95d5c0997d372ef8aa27c08b28883
andooown <andooown.dev@gmail.com>
2018-02-25 22:40:38 +0900

    ViewControllerのview上にMixiSampleViewControllerのviewを表示

--- 1.3/MyFirstProject/MyFirstProject/ViewController.m
+++ 1.3/MyFirstProject/MyFirstProject/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MixiSampleViewController.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // MixiSampleViewController
+    MixiSampleViewController *_sampleVC = [[MixiSampleViewController alloc] initWithNibName:@"MixiSampleViewController" bundle:nil];
+    [self.view addSubview:_sampleVC.view];
 }
 
 
