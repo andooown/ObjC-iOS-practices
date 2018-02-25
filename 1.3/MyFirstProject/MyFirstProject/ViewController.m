054addc7c0534e5d73f89448f45709244d1ce196
andooown <andooown.dev@gmail.com>
2018-02-25 22:18:22 +0900

    プロジェクトを作成

---
+++ 1.3/MyFirstProject/MyFirstProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFirstProject
+//
+//  Created by Yoshikazu Ando on 2018/02/25.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
