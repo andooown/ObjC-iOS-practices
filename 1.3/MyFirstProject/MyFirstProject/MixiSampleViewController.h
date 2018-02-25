b50146319855585f06907e71772a0aeebf8d7584
andooown <andooown.dev@gmail.com>
2018-02-25 22:28:14 +0900

    MixiSampleViewControllerを追加

---
+++ 1.3/MyFirstProject/MyFirstProject/MixiSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  MixiSampleViewController.h
+//  MyFirstProject
+//
+//  Created by Yoshikazu Ando on 2018/02/25.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiSampleViewController : UIViewController
+
+@end
---
+++ 1.3/MyFirstProject/MyFirstProject/MixiSampleViewController.m
@@ -0,0 +1,37 @@
+//
+//  MixiSampleViewController.m
+//  MyFirstProject
+//
+//  Created by Yoshikazu Ando on 2018/02/25.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import "MixiSampleViewController.h"
+
+@interface MixiSampleViewController ()
+
+@end
+
+@implementation MixiSampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
