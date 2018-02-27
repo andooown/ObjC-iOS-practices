3967925de93d03a0e887a2dde468dfc74e290e34
Ando Yoshikazu <andooown.dev@gmail.com>
2018-02-28 00:20:18 +0900

    ModalViewControllerにDismiss Modalボタンを押したときの処理を委譲するプロトコルの宣言と処理を追加

--- 1.4/MyFirstProject/MyFirstProject/ModalViewController.h
+++ 1.4/MyFirstProject/MyFirstProject/ModalViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+// プロトコルの宣言
+@protocol ModalViewControllerDelegate <NSObject>
+- (void)didPressDismissModalButton;     // Dismiss Modal ボタンが押されたときの処理
+@end
+
 @interface ModalViewController : UIViewController
+
+@property (nonatomic, weak) id<ModalViewControllerDelegate> delegate;   // delegate オブジェクト
 
 @end
