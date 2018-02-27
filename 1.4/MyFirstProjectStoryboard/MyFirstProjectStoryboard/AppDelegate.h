7e994dd95f7cc544d00c5542459cfc60d67ed7b6
Ando Yoshikazu <andooown.dev@gmail.com>
2018-02-28 00:26:39 +0900

    1.3のstoryboard編のプロジェクトをコピー

---
+++ 1.4/MyFirstProjectStoryboard/MyFirstProjectStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstProjectStoryboard
+//
+//  Created by Yoshikazu Ando on 2018/02/25.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
