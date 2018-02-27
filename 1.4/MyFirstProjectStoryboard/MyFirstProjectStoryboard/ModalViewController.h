d1a04aabdc94f3520d693e316911bf7307b455dc
Ando Yoshikazu <andooown.dev@gmail.com>
2018-02-28 00:30:50 +0900

    ModalViewControllerを作成してラベルを配置

---
+++ 1.4/MyFirstProjectStoryboard/MyFirstProjectStoryboard/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  MyFirstProjectStoryboard
+//
+//  Created by Ando Yoshikazu on 2018/02/28.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
