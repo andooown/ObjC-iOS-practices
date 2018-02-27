284e8e2a86db95b84f99205f32be8445dbb49e21
Ando Yoshikazu <andooown.dev@gmail.com>
2018-02-27 23:58:37 +0900

    ModalViewControllerを作成

---
+++ 1.4/MyFirstProject/MyFirstProject/ModalViewController.m
@@ -0,0 +1,37 @@
+//
+//  ModalViewController.m
+//  MyFirstProject
+//
+//  Created by Ando Yoshikazu on 2018/02/27.
+//  Copyright © 2018年 Ando Yoshikazu. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
