aec310b8db94163a06fdeb44457e18faf39f33a4
andooown <andooown.dev@gmail.com>
2018-03-01 01:08:12 +0900

    プロジェクトを作成

---
+++ Homeworks/1.2/ModalLoopProject/ModalLoopProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModalLoopProject
+//
+//  Created by Yoshikazu Ando on 2018/03/01.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homeworks/1.2/ModalLoopProject/ModalLoopProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ModalLoopProject
+//
+//  Created by Yoshikazu Ando on 2018/03/01.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
