47c758bbd7e274b32a5097b3d2fdbd08d0ec2dc1
andooown <andooown.dev@gmail.com>
2018-03-04 18:09:25 +0900

    プロジェクトを作成

---
+++ 2.2/TabbedAppProject/TabbedAppProject/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  TabbedAppProject
+//
+//  Created by Yoshikazu Ando on 2018/03/04.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
